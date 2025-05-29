2a0e6db801e0ced78d388f89af838987386ea058
victor <vicjosneros@gmail.com>
2025-05-28 21:52:50 -0600

    feat: update dependencies and restructure components
    - Added new dependencies for Netlify, React, Tailwind CSS, and Lucide React.
    - Removed the Welcome component and replaced it with a new Header and Hero component.
    - Introduced a global CSS file for Tailwind CSS styles.
    - Updated TypeScript configuration to support React JSX.
    - Added a new image asset for the Hero component.

---
+++ src/components/header.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <header className=" bg-[#145068]">
+            <nav className="mx-auto px-6 md:px-16 lg:px-24 xl:px-32 flex items-center justify-between py-4 md:py-5 relative">
+                <a href="#">
+                    <h1 className='text-white font-extrabold'>Nicaragua</h1>
+                </a>
+
+
+                <div className="md:hidden">
+                    <button id="menu-toggle" className="text-white focus:outline-none">
+                        <svg className="w-6 h-6" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round">
+                            <path d="M4 6h16M4 12h16M4 18h16" />
+                        </svg>
+                    </button>
+                </div>
+
+
+                <ul id="menu"
+                    className="hidden absolute top-full left-0 w-full bg-white md:static md:flex md:space-x-10 md:items-center text-gray-600 text-sm font-medium md:w-auto md:bg-transparent md:mt-0 mt-4 shadow md:shadow-none z-10 px-6 py-4 md:py-0">
+                    <li><a href="#" className="block text-white py-2 hover:text-gray-200 transition">Turismo</a></li>
+                    <li><a href="#" className="block text-white py-2 hover:text-gray-200 transition">Gastronomia</a></li>
+                    <li><a href="#" className="block text-white py-2 hover:text-gray-200 transition">Cultura</a></li>
+                    <li><a href="#" className="block text-white py-2 hover:text-gray-200 transition">Tradiciones</a></li>
+
+                </ul>
+
+
+            </nav>
+        </header>
+    )
+}
